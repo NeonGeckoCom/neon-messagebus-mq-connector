2df5114190b2d5b5cdf21c84caac7d6cb52a0fdf
NeonBohdan <bohdan@neon.ai>
2022-01-05 20:07:56 +0200

    messages migrated to pydantic

--- chat_api_mq_proxy/messages.py
+++ chat_api_mq_proxy/messages.py
@@ -1,26 +1,16 @@
+from pydantic import BaseModel, create_model
+
+
+class STT(BaseModel):
+    msg_type: str
+    data: create_model("Data",
+        audio_file = (str, ...),
+        lang = (str, ...)
+    )
+    context: create_model("Context"
+    )
+
+
 templates = {
-    "recognizer_loop:utterance": {
-        "msg_type": str,
-        "data": {
-            "audio_file": str,
-            "lang": str
-        },
-        "context":{
-            "ident": str
-        }
-    },
-    "klat.shout": {
-        "msg_type": str,
-        "data": {
-            "sid": str,
-            "nick": str,
-            "cid": str,
-            "time": str,
-            "title": str,
-            "text": str
-        },
-        "context": {
-            "neon_should_respond": bool
-        }
-    }
+    "recognizer_loop:utterance": STT,
 }
