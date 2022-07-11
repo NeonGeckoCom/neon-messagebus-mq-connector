98ffbcca777e6b267d16d004f12e5ccaad7df1c9
NeonKirill <kirill.grim@gmail.com>
2022-07-11 18:13:08 +0200

    cleaned up message validation structure

--- neon_messagebus_mq_connector/controller.py
+++ neon_messagebus_mq_connector/controller.py
@@ -25,7 +25,7 @@
 # LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE,  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
+from typing import List, Type, Tuple
 
 import pika
 from mycroft_bus_client import MessageBusClient, Message
@@ -37,7 +37,7 @@ from neon_mq_connector.connector import MQConnector
 from pika.channel import Channel
 from pydantic import ValidationError
 
-from .messages import templates
+from .messages import templates, BaseModel
 
 
 class ChatAPIProxy(MQConnector):
@@ -47,7 +47,7 @@ class ChatAPIProxy(MQConnector):
     def __init__(self, config: dict, service_name: str):
         super().__init__(config, service_name)
 
-        self.vhost = '/neon_chat_api'
+        self._vhost = '/neon_chat_api'
         self.bus_config = config.get('MESSAGEBUS') or \
             dict(Configuration()).get("websocket")
         self._bus = None
@@ -57,13 +57,13 @@ class ChatAPIProxy(MQConnector):
                                queue=f'neon_chat_api_request_{self.service_id}',
                                callback=self.handle_user_message,
                                on_error=self.default_error_handler,
-                               auto_ack=False)
+                               auto_ack=True)
         self.register_consumer(name='neon_request_consumer',
                                vhost=self.vhost,
                                queue='neon_chat_api_request',
                                callback=self.handle_user_message,
                                on_error=self.default_error_handler,
-                               auto_ack=False)
+                               auto_ack=True)
 
     def register_bus_handlers(self):
         """Convenience method to gather message bus handlers"""
@@ -115,10 +115,7 @@ class ChatAPIProxy(MQConnector):
         LOG.debug(f'Received neon response body: {body}')
         routing_key = message.context.get("klat", {}).get("routing_key") or \
             routing_key or 'neon_chat_api_response'
-        with self.create_mq_connection(vhost=self.vhost) as mq_connection:
-            self.emit_mq_message(connection=mq_connection,
-                                 request_data=body,
-                                 queue=routing_key)
+        self.send_message(request_data=body, queue=routing_key)
 
     def handle_neon_profile_update(self, message: Message):
         """
@@ -134,36 +131,57 @@ class ChatAPIProxy(MQConnector):
             LOG.debug(f"ignoring profile update for "
                       f"user={message.data['profile']['user']['username']}")
 
-    def validate_request(self, dict_data: dict):
+    @staticmethod
+    def __validate_message_templates(msg_data: dict, message_templates: List[Type[BaseModel]] = None) -> Tuple[str, dict]:
         """
-        Validate dict_data dictionary structure by using tamplate
-        All templates are located in messages.py file
+            Validate selected pydantic message templates into provided message data
 
-        :param dict_data: request for validation
-        :return: validation details(None if validation passed),
-                 input data with proper data types and filled default fields
+            :param msg_data: Message data to fetch
+            :param message_templates: list of pydantic templates to fetch into data
+
+            :returns tuple containing 2 values:
+                     1) validation error if detected;
+                     2) fetched message data;
         """
-        def check_keys_presence(dict_data, message_templates):
+
+        if not message_templates:
+            LOG.warning('No matching templates found, skipping template fetching')
+            return '', msg_data
+
+        LOG.info('Initiating template validation')
+        for message_template in message_templates:
             try:
-                for message_template in message_templates:
-                    dict_data = message_template(**dict_data).dict()
+                msg_data = message_template(**msg_data).dict()
             except (ValueError, ValidationError) as err:
-                return err, dict_data
-            return None, dict_data
+                LOG.error(f'Failed to validate {msg_data} with template = {message_template.__name__}, exception={err}')
+                return str(err), msg_data
+        LOG.info('Template validation completed successfully')
+        return '', msg_data
 
-        # TODO: This is really `templates`, not `skills`
-        request_skills = dict_data["context"].get("request_skills",
-                                                  ["default"])
-        if len(request_skills) == 0:
-            request_skills = ["default"]
-        try:
-            message_templates = [templates[request_type]
-                                 for request_type in request_skills]
-        except KeyError:
-            return None, dict_data
-        check_error, dict_data = check_keys_presence(dict_data,
-                                                     message_templates)
-        return check_error, dict_data
+    @classmethod
+    def validate_request(cls, msg_data: dict):
+        """
+            Fetches the relevant template models and validates provided message data iteratively through them
+
+            :param msg_data: message data for validation
+
+            :return: validation details(None if validation passed),
+                     input data with proper data types and filled default fields
+        """
+
+        requested_templates = msg_data.get("context", {}).get("request_skills") or ["default"]
+        message_templates = []
+
+        for requested_template in requested_templates:
+            matching_template_model = templates.get(requested_template)
+            if not matching_template_model:
+                LOG.warning(f'Template under keyword "{requested_template}" does not exist')
+            else:
+                message_templates.append(matching_template_model)
+
+        detected_error, msg_data = cls.__validate_message_templates(msg_data=msg_data,
+                                                                    message_templates=message_templates)
+        return detected_error, msg_data
 
     def handle_user_message(self,
                             channel: pika.channel.Channel,
@@ -171,30 +189,23 @@ class ChatAPIProxy(MQConnector):
                             properties: pika.spec.BasicProperties,
                             body: bytes):
         """
-        Handles requests from MQ to Neon Chat API received on queue
-        "neon_chat_api_request"
+            Transfers requests from MQ API to Neon Message Bus API
 
-        :param channel: MQ channel object (pika.channel.Channel)
-        :param method: MQ return method (pika.spec.Basic.Return)
-        :param properties: MQ properties (pika.spec.BasicProperties)
-        :param body: request body (bytes)
+            :param channel: MQ channel object (pika.channel.Channel)
+            :param method: MQ return method (pika.spec.Basic.Return)
+            :param properties: MQ properties (pika.spec.BasicProperties)
+            :param body: request body (bytes)
 
         """
         if body and isinstance(body, bytes):
             dict_data = b64_to_dict(body)
             LOG.info(f'Received user message: {dict_data}')
-            dict_data["context"].setdefault("mq", dict())
-            if "routing_key" in dict_data:
-                dict_data["context"]["mq"]["routing_key"] = \
-                    dict_data.pop("routing_key")
-            if "message_id" in dict_data:
-                dict_data["context"]["mq"]["message_id"] = \
-                    dict_data.pop("message_id")
-            check_error, dict_data = self.validate_request(dict_data)
-            if check_error is not None:
-                LOG.error(check_error)
+            dict_data["context"].setdefault("mq", dict(routing_key=dict_data.pop('routing_key', ''),
+                                                       message_id=dict_data.pop('message_id', '')))
+            validation_error, dict_data = self.validate_request(dict_data)
+            if validation_error:
                 response = Message(msg_type="klat.error",
-                                   data=dict(error=str(check_error),
+                                   data=dict(error=validation_error,
                                              message=dict_data))
                 self.handle_neon_message(response, "neon_chat_api_error")
             else:
